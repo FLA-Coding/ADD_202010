0e37d517156d8a45b9990539ec86e2b7ff9417d7
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-03 12:58:36 +0100

    Passage sur un fichier sh pour PyInstaller (macOS)

---
+++ pyinstaller_macos.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 -m PyInstaller --onefile --noconsole --icon ADD_logo.ico --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' tkinter_client_mac.pyw
