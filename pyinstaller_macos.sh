8b8c5fe24d707ae679dcbfc3d03665f826506875
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-03 12:25:08 +0000

    Ajout des --add-binary supprimés par erreur

--- pyinstaller_macos.sh
+++ pyinstaller_macos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 -m PyInstaller --onefile --noconsole --icon ADD_logo.ico --name "Générateur d\'attestation de déplacement dérogatoire" tkinter_client_lnx.pyw 
+python3 -m PyInstaller --onefile --noconsole --icon ADD_logo.ico --name "Générateur d\'attestation de déplacement dérogatoire" --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' tkinter_client_lnx.pyw 
