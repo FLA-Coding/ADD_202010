e02aa7c41aaa85a0160160350343d11c97fff228
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-02 16:11:45 -0800

    Petits fichiers divers (compilation macOS, logo)

---
+++ build_dmg.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Placer le .app dans un sous-dossier « source_folder »
+
+test -f Application-Installer.dmg && rm Application-Installer.dmg
+create-dmg \
+  --volname "Générateur d’attestation de déplacement dérogatoire" \
+  --volicon "application_icon.icns" \
+  --background "dmg_background.png" \
+  --window-pos 200 120 \
+  --window-size 700 600 \
+  --icon-size 150 \
+  --icon "Générateur d'attestation de déplacement dérogatoire.app" 150 330 \
+  --hide-extension "Générateur d'attestation de déplacement dérogatoire.app" \
+  --app-drop-link 550 330 \
+  "ADD_202010_macOS_amd64_1-1.dmg" \
+  "source_folder/"
