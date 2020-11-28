5be9e7de1a0ac230b4e1d6ea72f61372678540a7
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-28 12:53:04 +0100

    Correction nom fichier

--- build_dmg.sh
+++ build_dmg.sh
@@ -5,13 +5,13 @@
 test -f Application-Installer.dmg && rm Application-Installer.dmg
 create-dmg \
   --volname "Générateur d’attestation de déplacement dérogatoire" \
-  --volicon "application_icon.icns" \
+  --volicon "ADD_logo.icns" \
   --background "dmg_background.png" \
   --window-pos 200 120 \
   --window-size 700 600 \
   --icon-size 150 \
-  --icon "Générateur d'attestation de déplacement dérogatoire.app" 150 330 \
-  --hide-extension "Générateur d'attestation de déplacement dérogatoire.app" \
+  --icon "Générateur d'attestation de déplacement dérogatoire.app" 150 330 \
+  --hide-extension "Générateur d'attestation de déplacement dérogatoire.app" \
   --app-drop-link 550 330 \
   "ADD_202010_macOS_amd64_1-1.dmg" \
   "source_folder/"
